b3e0889f89dbe094a85b55a207d84b508c230ccd
Tetereka <74609981+Tetereka@users.noreply.github.com>
2022-02-10 21:36:29 +0300

    first cm

---
+++ header.h
@@ -0,0 +1,9 @@
+#ifndef HEADER_H_INCLUDED
+#define HEADER_H_INCLUDED
+#include <iostream>
+#include <SDL2/SDL.h>
+
+void it_horizontalLine(SDL_Renderer *render, int x, int y, int length);
+void it_verticalLine(SDL_Renderer *render, int x, int y, int length);
+
+#endif // HEADER_H_INCLUDED
